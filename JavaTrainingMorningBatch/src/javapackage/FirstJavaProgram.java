6934d7370f2a0ad4dec79a61aa55bcb6020f5f87
DEEPAKKAMDE <deepakkamde03@gmail.com>
2025-03-15 08:40:47 +0530

    TestJenkins

---
+++ JavaTrainingMorningBatch/src/javapackage/FirstJavaProgram.java
@@ -0,0 +1,14 @@
+package javapackage;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+
+	}
+
+}
+
+
